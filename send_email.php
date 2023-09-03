205b65d4a3a74f7723f9dd9ea234f394f2380e4e
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2023-09-03 12:19:45 +0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -2,15 +2,29 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-  $to = "adnan.azem.250@gmail.com"; // Your email address
-  $subject = "Contact Form Submission from $name";
-  $headers = "From: $email";
+    // Set the recipient email address
+    $to = "adnan.azem.250@gmail.com"; // Replace with the recipient's email address
 
-  // Send the email
-  mail($to, $subject, $message, $headers);
+    // Set email subject
+    $subject = "Contact Form Submission";
+
+    // Compose the email message
+    $email_message = "Name: " . $name . "\n";
+    $email_message .= "Email: " . $email . "\n";
+    $email_message .= "Message:\n" . $message;
+
+    // Additional headers
+    $headers = "From: " . $email . "\r\n";
+
+    // Send the email
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again later.";
+    }
 }
 ?>
